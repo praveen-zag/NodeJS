53bd1918134b9afba6c582c6133fbf184e951a1a
praveen-zag <praveensingh190720@gmail.com>
2024-11-19 20:08:08 +0530

    day-3

---
+++ 4-names.js
@@ -0,0 +1,9 @@
+//local
+const secret='Secret'
+//shared
+const john='John'
+const peter='peter'
+
+//to only share essential names
+module.exports= {john,peter}
+// console.log(module);
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,9 @@
+//to export directly
+//module.exports.items=['item1','item2'] 
+const items=['item1','item2']
+const person={
+    name:'bob',
+}
+
+module.exports = items
+module.exports.singlePerson= person
